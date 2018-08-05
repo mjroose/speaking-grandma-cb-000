1e48c0643f4017b5d272076a489669a23be062ad
Michael Roose <mjroose@gmail.com>
2018-08-05 15:23:18 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,13 @@
 # Write a speak_to_grandma method.
 
+def speak_to_grandma(phrase)
+  if (phrase != phrase.upcase)
+    "HUH?! SPEAK UP, SONNY!"
+  else
+
+  end
+end
+
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
